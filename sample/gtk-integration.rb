dadbd340eb2fd709e44687f34db6d15f04c284cc
Kouhei Sutou <kou@clear-code.com>
2018-05-24 18:06:24 +0900

    Make GTK+ integration sample workable

--- sample/gtk-integration.rb
+++ sample/gtk-integration.rb
@@ -7,18 +7,22 @@ app = Gtk::Application.new
 app.signal_connect(:activate) do
   window = Gtk::ApplicationWindow.new(app)
   image = Gtk::Image.new
+  window.add(image)
+
   camera = CV::Camera.new
-  GLib::Timeout.add(100) do
+  update_id = GLib::Idle.add do
     cv_image = camera.read
     cv_image_rgb = cv_image.convert_color(:bgr2rgb)
     image.pixbuf = GdkPixbuf::Pixbuf.new(bytes: cv_image_rgb.bytes,
-                                         has_alpha: cv_image_rgb.n_channels == 4,
-                                         width: cv_iamge_rgb.n_columns,
-                                         height: cv_iamge_rgb.n_rows)
+                                         width: cv_image_rgb.n_columns,
+                                         height: cv_image_rgb.n_rows)
     image.queue_draw
     GLib::Source::CONTINUE
   end
-  window.add(image)
+  window.signal_connect(:destroy) do
+    GLib::Source.remove(update_id)
+  end
+
   window.show_all
 end
 app.run
